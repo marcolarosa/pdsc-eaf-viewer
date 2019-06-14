61106f05a4173237e56e1312dcb422ecf17ec8a2
Marco La Rosa <m@lr.id.au>
2019-06-14 10:57:01 +1000

    implement and wire up issues display

---
+++ src/components/RenderIssues.component.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-table :data="issues" :height="height" stripe>
+            <el-table-column prop="msg" label="Error" sortable></el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        issues: {
+            type: Array
+        }
+    },
+    data() {
+        return {
+            height: 300
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
--- src/components/Shell.component.vue
+++ src/components/Shell.component.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="container mx-auto font-mono p-4">
-        <!-- <pre>{{index}}</pre> -->
-        <render-data-table-component/>
+        <render-data-table-component v-on:row-selected="loadFileData"/>
+        <div class="flex">
+            <div class="w-1/2">
+                <render-issues-component :issues="dataFile.issues" v-if="dataFile"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import { loadData } from "../data-loader.service.js";
+import { loadIndex, loadFileData } from "../data-loader.service.js";
 import RenderDataTableComponent from "./RenderDataTable.component.vue";
+import RenderIssuesComponent from "./RenderIssues.component.vue";
 
 export default {
     components: {
-        RenderDataTableComponent
+        RenderDataTableComponent,
+        RenderIssuesComponent
     },
     data() {
-        return {};
+        return {
+            dataFile: undefined
+        };
     },
     computed: {
         index: function() {
@@ -23,8 +31,13 @@ export default {
     },
     mounted() {
         (async () => {
-            await loadData({ store: this.$store });
+            await loadIndex({ store: this.$store });
         })();
+    },
+    methods: {
+        async loadFileData(file) {
+            this.dataFile = await loadFileData({ dataFile: file.dataFile });
+        }
     }
 };
 </script>
