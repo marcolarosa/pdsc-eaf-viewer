45561dea592cdd3e43d25f217f9d1567afa49be7
Marco La Rosa <m@lr.id.au>
2019-06-14 10:24:06 +1000

    move table to own component and add file filter capability

---
+++ src/components/RenderDataTable.component.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <el-form label-width="200px">
+            <el-form-item label="Filter files">
+                <el-input v-model="fileFilter" type="text" placeholder="Filter file list..."></el-input>
+            </el-form-item>
+        </el-form>
+        <el-table
+            :data="index"
+            :height="height"
+            width="800"
+            :default-sort="{prop: 'file', order: 'ascending'}"
+            stripe
+            class="text-lg"
+        >
+            <el-table-column prop="file" label="File" width="500" fixed sortable></el-table-column>
+            <el-table-column prop="start" label="Start Time" :width="dataColumnWidth"></el-table-column>
+            <el-table-column prop="end" label="End Time" :width="dataColumnWidth"></el-table-column>
+            <el-table-column prop="duration" label="Duration" :width="dataColumnWidth" sortable></el-table-column>
+            <el-table-column prop="timeslots" label="# Timeslots" :width="dataColumnWidth" sortable></el-table-column>
+            <el-table-column
+                prop="totalRefAnnotations"
+                label="# Ref Annotations"
+                :width="dataColumnWidth"
+                sortable
+            ></el-table-column>
+            <el-table-column
+                prop="totalMappedAnnotations"
+                label="# Mapped Annotations"
+                :width="dataColumnWidth"
+                sortable
+            ></el-table-column>
+            <el-table-column prop="totalIssues" label="Issues" :width="dataColumnWidth" sortable></el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            height: 500,
+            dataColumnWidth: 280,
+            fileFilter: undefined
+        };
+    },
+    computed: {
+        index: function() {
+            return this.$store.state.index.filter(f =>
+                f.file.match(this.fileFilter)
+            );
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/Shell.component.vue
+++ src/components/Shell.component.vue
@@ -1,45 +1,20 @@
 <template>
-    <div class="container mx-auto font-mono">
+    <div class="container mx-auto font-mono p-4">
         <!-- <pre>{{index}}</pre> -->
-        <el-table
-            :data="index"
-            :height="height"
-            width="800"
-            :default-sort="{prop: 'file', order: 'ascending'}"
-            stripe
-            class="text-lg"
-        >
-            <el-table-column prop="file" label="File" width="500" fixed sortable></el-table-column>
-            <el-table-column prop="start" label="Start Time" :width="dataColumnWidth"></el-table-column>
-            <el-table-column prop="end" label="End Time" :width="dataColumnWidth"></el-table-column>
-            <el-table-column prop="duration" label="Duration" :width="dataColumnWidth" sortable></el-table-column>
-            <el-table-column prop="timeslots" label="# Timeslots" :width="dataColumnWidth" sortable></el-table-column>
-            <el-table-column
-                prop="totalRefAnnotations"
-                label="# Ref Annotations"
-                :width="dataColumnWidth"
-                sortable
-            ></el-table-column>
-            <el-table-column
-                prop="totalMappedAnnotations"
-                label="# Mapped Annotations"
-                :width="dataColumnWidth"
-                sortable
-            ></el-table-column>
-            <el-table-column prop="totalIssues" label="Issues" :width="dataColumnWidth" sortable></el-table-column>
-        </el-table>
+        <render-data-table-component/>
     </div>
 </template>
 
 <script>
 import { loadData } from "../data-loader.service.js";
+import RenderDataTableComponent from "./RenderDataTable.component.vue";
 
 export default {
+    components: {
+        RenderDataTableComponent
+    },
     data() {
-        return {
-            height: 500,
-            dataColumnWidth: 280
-        };
+        return {};
     },
     computed: {
         index: function() {
